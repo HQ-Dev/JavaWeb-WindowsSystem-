04c5969a3960430de56bf45d52fe2e26c81f1105
HQ <HQ_Dev@163.com>
2015-12-17 00:42:46 +0800

    JavaWeb
    前两周三道编程题

---
+++ 多大的/src/多大的/Main.java
@@ -0,0 +1,15 @@
+package ¶à´óµÄ;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int F = in.nextInt();
+		double C = (F-32)*(5/9.0);
+		System.out.print((int)C);
+	}
+
+}
